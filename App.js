141ad3eefc875b11570f6cdeb2eb314f66b2f72d
montassar.bouhjila <montassar.bouhjila@gmail.com>
2020-10-05 22:55:40 +0100

    persistance

--- App.js
+++ App.js
@@ -19,24 +19,28 @@ import Store from './app/store/configureStore';
 import searchFoodScreen from './app/component/searchFoodScreen';
 import FoodDetailScreen from './app/component/foodDetailScreen';
 import FavotireFoodScreen from './app/component/favoriteFood';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
+  let persistor = Store.persistor;
   return (
     <>
-      <Provider store={Store}>
-        <NavigationContainer>
-          <StatusBar barStyle="dark-content" />
-           <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Food" component={searchFoodScreen} />
-            <Stack.Screen name="FoodDetail" component={FoodDetailScreen} />
-            <Stack.Screen name="Favorite" component={FavotireFoodScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="About" component={AboutScreen} />
-            <Stack.Screen name="Form" component={FormScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
+      <Provider store={Store.store}>
+        <PersistGate persistor={persistor}>
+          <NavigationContainer>
+            <StatusBar barStyle="dark-content" />
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen name="Food" component={searchFoodScreen} />
+              <Stack.Screen name="FoodDetail" component={FoodDetailScreen} />
+              <Stack.Screen name="Favorite" component={FavotireFoodScreen} />
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="About" component={AboutScreen} />
+              <Stack.Screen name="Form" component={FormScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PersistGate>
       </Provider>
     </>
   );
--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,7 +1,24 @@
-import {createStore, combineReducers} from 'redux';
+/* eslint-disable prettier/prettier */
+import {createStore} from 'redux';
 import userReducer from '../reducer/userReducer';
 import foodReducer from '../reducer/foodReducer';
+import {persistCombineReducers, persistStore} from 'redux-persist';
+import createAsyncStorage from 'redux-persist-react-native-async-storage';
 
-export default createStore(
-  combineReducers({user: userReducer, food: foodReducer}),
-);
+const storage = createAsyncStorage();
+
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+};
+const reducer = persistCombineReducers(rootPersistConfig, {
+  user: userReducer,
+  food: foodReducer,
+});
+const configureStore = () => {
+  const store = createStore(reducer);
+  const persistor = persistStore(store);
+
+  return { persistor, store };
+};
+export default configureStore();
