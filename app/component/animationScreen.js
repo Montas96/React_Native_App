a94da6e40e7fcc94050824821219204b64d49d92
montassar.bouhjila <montassar.bouhjila@gmail.com>
2020-10-08 20:35:01 +0100

    animated parallel

--- app/component/animationScreen.js
+++ app/component/animationScreen.js
@@ -2,58 +2,59 @@
 // Components/Test.js
 
 import React from 'react';
-import { StyleSheet, View, Animated, Easing  } from 'react-native';
+import { StyleSheet, View, Animated, Easing } from 'react-native';
 
 class AnimationScreen extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        topPosition: new Animated.Value(0),
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            topPosition: new Animated.Value(0),
+            leftPosition: new Animated.Value(0),
+        };
+    }
 
-  componentDidMount() {
-    Animated.sequence([
-      Animated.spring(
-        this.state.topPosition,
-        {
-          toValue: 100,
-          tension: 8,
-          friction: 3,
-        }
-      ),
-      Animated.timing(
-        this.state.topPosition,
-        {
-          toValue: 0,
-          duration: 1000,
-          easing: Easing.elastic(2),
-        }
-      ),
-    ]).start();
-}
+    componentDidMount() {
+        Animated.parallel([
+            Animated.spring(
+                this.state.topPosition,
+                {
+                    toValue: 100,
+                    tension: 8,
+                    friction: 3,
+                }
+            ),
+            Animated.timing(
+                this.state.leftPosition,
+                {
+                    toValue: 100,
+                    duration: 1000,
+                    easing: Easing.elastic(2),
+                }
+            ),
+        ]).start();
+    }
 
-  render() {
-    return (
-      <View style={styles.main_container}>
-        <Animated.View style={[styles.animation_view, { top: this.state.topPosition }]} />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <Animated.View style={[styles.animation_view, { top: this.state.topPosition, left: this.state.leftPosition }]} />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  main_container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  animation_view: {
-    backgroundColor: 'red',
-    width: 100,
-    height: 100,
-  },
+    main_container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    animation_view: {
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+    },
 });
 
 export default AnimationScreen;
